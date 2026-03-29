e85e0d14bc461d100348622e03e23d28f26dbbd9
Amarrish <amarrishms732@gmail.com>
2026-03-29 16:56:16 +0530

    deploy

--- Frontend/vite-project/src/components/Admin-view/OrderDetails.jsx
+++ Frontend/vite-project/src/components/Admin-view/OrderDetails.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState } from "react";
-import CommonForm from "../common/form";
+import CommonForm from "../Common/Form";
 import { DialogContent } from "../ui/dialog";
 import { Label } from "../ui/label";
 import { Separator } from "../ui/separator";
